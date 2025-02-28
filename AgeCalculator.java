d5c475bb2c69c2b48c9e6677d9efcc0807c1231e
Joshua George Pangilinan <Joshuagpan@gmail.com>
2025-02-28 10:09:13 -0500

    Initial sharing of project

---
+++ AgeCalculator.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class AgeCalculator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AgeCalculator
+{
+ private int currentYear = 2025;
+ 
+ if age >= 16 ableToDrive {
+     
+     else   age =< 16 unableToDrive
+     
+     System.out.println("You have   years to legally drive");
+     
+ }
+}
---
+++ Salary.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Salary here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Salary
+{
+    private SalaryCalculator(){
+        private int= hours, hourly wage;
+    }
+}
